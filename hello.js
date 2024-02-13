85002ef4f109db1feb9cf61a6134ddaa626e372d
Saikat5943 <159873989+Saikat5943@users.noreply.github.com>
2024-02-13 11:26:43 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+
+function fun(){
+    const element = document.getElementById('sai');
+    const utr=element.innerHTML;
+    if(utr==='saikat'){
+        element.innerHTML="hello";
+    }
+    else{
+        element.innerHTML="saikat";
+    }
+}
